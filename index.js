01badfe91d43dda5dd0439b7b8c15cccbe770783
unknown <irfanulislamfahim@gmail.com>
2022-05-27 21:09:23 +0600

    admin added

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     console.log('mongodb connected')
-//     client.close();
-// });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('mongodb connected')
+    client.close();
+});
 
 
 async function run() {
