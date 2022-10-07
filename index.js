e82d498e49a308835362773885c4aa99e603f229
Fahimphero <irfanulislamfahim@gmail.com>
2022-10-08 03:15:38 +0600

    delete user api added

--- index.js
+++ index.js
@@ -228,6 +228,16 @@ async function run() {
             }
 
         })
+
+        // Delete User
+        app.delete('/users/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
     }
 
     finally {
